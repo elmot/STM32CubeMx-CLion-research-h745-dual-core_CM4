3f49a2dc827f45f1020edcfbe55b432bc4c647be
Ilia (Elias) Motornyi <elijah.mot@gmail.com>
2024-11-08 12:26:25 +0200

    Desired STM32CubeMX changes

--- CM7/Core/Src/main.c
+++ CM7/Core/Src/main.c
@@ -106,7 +106,11 @@ int main(void)
 /* USER CODE BEGIN Boot_Mode_Sequence_1 */
   /* Wait until CPU2 boots and enters in stop mode or timeout*/
   timeout = 0xFFFF;
-  while((__HAL_RCC_GET_FLAG(RCC_FLAG_D2CKRDY) != RESET) && (timeout-- > 0));
+  while(__HAL_RCC_GET_FLAG(RCC_FLAG_D2CKRDY) != RESET) {
+#ifndef DEBUG
+      if(timeout-- <= 0) break;
+#endif
+  }
   if ( timeout < 0 )
   {
   Error_Handler();
